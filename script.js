308d964adb08fea65308c0503cf1fac72f8babc2
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-02-29 22:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 //your JS code here. If required.
-const line = document.getElementById('line');
+const line = document.getElementById("line");
 let angle = 0;
-
-function rotateLine(timestamp) {
-  angle += 0.2; // Adjust speed here
-  line.style.transform = `rotate(${angle}deg)`;
-  requestAnimationFrame(rotateLine);
+function rotateLine() {
+	angle += 2;
+	line.style.transform = `rotate(${angle}deg)`;
+	
 }
-
-requestAnimationFrame(rotateLine);
+setTimeout(rotateLine, 20);
+// rotateLine();
